bb52f39695fc2a620f97f2f9fbbdfb9e224c43b3
prempachkawade <pachkawadeprem780@gmail.com>
2022-06-09 14:55:10 +0530

    first commit

---
+++ basic.php
@@ -0,0 +1,79 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>basic</title>
+	<script type="text/javascript">
+		
+
+		let a=5;
+		let b=8;
+		let c= 5/8;
+		alert(c);
+
+		var m=45;
+		var n=15;
+		var o=m*n;
+		alert(o);
+
+
+
+
+
+
+
+
+	</script>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
